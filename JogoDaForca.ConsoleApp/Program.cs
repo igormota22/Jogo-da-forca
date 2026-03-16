425a250dc2dc58d2cbdc5b0ce6b370137875c363
Igor Mota <igormello399@gmail.com>
2026-03-16 15:36:25 -0300

    first commit

---
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program{
+    public static void Main(string[] args)
+{
+   System.Console.WriteLine("------------------------");
+   System.Console.WriteLine("JOGO DA FORCA");
+   System.Console.WriteLine("------------------------"); 
+
+}
+
+}
+
